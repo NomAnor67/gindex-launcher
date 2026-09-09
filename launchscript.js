b824944be794682481f61d1f764e1b5d63c640ff
Pierce <NomAnor67@protonmail.com>
2026-09-08 18:19:08 -0600

    v2.0.1

--- launchscript.js
+++ launchscript.js
@@ -21,6 +21,8 @@ function openCenteredPopup2() {
                     <div class="popup-content">
                         <button class="popup-close" onclick="closePatchPopup(event)">Close</button>
                         <div class="centered">
+                        <p>Note: Not all versions will get a patch notes update</p>
+                        <div></div>
                             <h1>v2.0</h1>
                             <pre>Added QOL features such as a patch notes dropdown, information regarding loading a website, and </pre>
                             <pre style="color:red;"> EXPERIMENTAL: </pre> <pre> Added GameIndex drivers and a list of tested links. Either of these features may be removed in the future.</pre>
